1b85264209094b4f472badea9e60a1b39d4e8d64
MishelAtaev <mishelataev@gmail.com>
2024-04-14 17:18:52 -0400

    basic navbar added

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+const NavBar = () => {
+  return (
+    <>
+      <nav>
+        <div>
+          <p>insert logo here</p>
+        </div>
+        <div>
+          <p>shop all</p>
+          <p>categories</p>
+          <p>contact</p>
+          <p>faq</p>
+        </div>
+        <div>
+          <p>cart icon here</p>
+          <p>user sign in icon here</p>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
