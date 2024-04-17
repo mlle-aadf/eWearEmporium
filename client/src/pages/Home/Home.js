bc0812a00b622950ef57afa8a76c81ea41cef437
MishelAtaev <mishelataev@gmail.com>
2024-04-17 16:16:50 -0400

    start of navbar

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,29 +1,31 @@
-import Footer from "../../components/Footer/Footer"
+import Footer from "../../components/Footer/Footer";
+import NavBar from "../../components/NavBar/NavBar";
 
 const Home = () => {
+  return (
+    // BASIC LAYOUT :
+    // [ ] navbar
+    // [ ] hero section, button redirects to PRODUCTS
+    // [ ] categories, also redirects to PRODUCTS > to pre-selected category
+    // [ ] rotating brands carousel, icons link to external brand websites,“see all” button links to BRANDS
+    // [ ] footer section
 
-    return(
-// BASIC LAYOUT :
-// [ ] navbar
-// [ ] hero section, button redirects to PRODUCTS
-// [ ] categories, also redirects to PRODUCTS > to pre-selected category
-// [ ] rotating brands carousel, icons link to external brand websites,“see all” button links to BRANDS 
-// [ ] footer section 
+    // [ ] cart
+    // [ ] email-signup/promo pop-up
 
-// [ ] cart 
-// [ ] email-signup/promo pop-up
-    
-<>
-<main className="main-page">
-<h1>E-WEAR <br/> EMPORIUM</h1>
-<br/>
-<h2>"Insert Hero section here"</h2>
-<br/>
-<Footer />
-</main>
-</>
-    )
+    <>
+      <NavBar />
+      <main className="main-page">
+        <h1>
+          E-WEAR <br /> EMPORIUM
+        </h1>
+        <br />
+        <h2>"Insert Hero section here"</h2>
+        <br />
+        <Footer />
+      </main>
+    </>
+  );
+};
 
-}
-
-export default Home
+export default Home;
