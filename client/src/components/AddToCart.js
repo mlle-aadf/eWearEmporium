9d77c91d74297ea3663bad5766f81b07cff413e3
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2024-04-25 19:01:19 -0400

    add to cart btn display text

--- client/src/components/AddToCart.js
+++ client/src/components/AddToCart.js
@@ -3,13 +3,23 @@ import { useEffect } from "react";
 import { CartContentContext } from "./Cart/CartContentContext";
 import { useContext } from "react";
 
+// updates item inventory in database
 const AddToCart = ({ item, inStock, setInStock }) => {
   const { addToCart, cart } = useContext(CartContentContext);
   useEffect(() => {
     setInStock(inStock);
   }, [inStock]);
 
+// changes "add to cart" button text depending on status
+  const itemAdded = (status) => {
+    document.getElementById("addBTN").innerText = status
+    
+    setTimeout(()=> document.getElementById("addBTN").innerText = "Add to cart", 5000)
+}
+
+// adds item to cart and changes button text
   const handleClick = async () => {
+    itemAdded("Adding item...")
     try {
       const response = await fetch(`/products/${item._id}`, {
         method: "PATCH",
@@ -17,23 +27,27 @@ const AddToCart = ({ item, inStock, setInStock }) => {
           "Content-Type": "application/json",
         },
       });
-
+      
       const data = await response.json();
-
+      
       if (response.ok) {
         addToCart(data);
         console.log("Item added to cart:", cart);
+        itemAdded("Added to cart!")
       } else {
+        itemAdded("Error")
         console.error("Failed to fetch item:", data.message);
       }
     } catch (error) {
       console.error("Error adding item to cart:", error);
     }
+
   };
 
   return (
     <AddBTN
       onClick={handleClick}
+      id="addBTN"
       className={inStock ? "addToCart" : "addToCartDisabled"}
       disabled={!inStock}
     >
