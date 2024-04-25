53ad954394374e1239c399b30fb905b1515245f6
MishelAtaev <mishelataev@gmail.com>
2024-04-25 19:37:55 -0400

    home page responsivness done

--- client/src/pages/Home/slidesData.js
+++ client/src/pages/Home/slidesData.js
@@ -2,6 +2,10 @@ import actionSlide from "../../images/ActionSlide.png";
 import fitnessSlide from "../../images/FitnessSlide.png";
 import healthSlide from "../../images/HealthSlide.png";
 import styleSlide from "../../images/StyleSlide.png";
+import actionMobile from "../../images/action_mobile.png";
+import fitnessMobile from "../../images/fitness_mobile.png";
+import healthMobile from "../../images/health_mobile.png";
+import styleMobile from "../../images/style_mobile.png";
 
 export const slidesData = [
   {
@@ -23,5 +27,28 @@ export const slidesData = [
     description:
       "Style meets innovation with devices designed to enhance every aspect of your lifestyle.",
     imageURL: styleSlide,
+  },
+];
+
+export const slidesMobile = [
+  {
+    description:
+      "Capture all the action in stunning detail with the ultimate adventure companion",
+    imageURL: actionMobile,
+  },
+  {
+    description:
+      "Elevate your fitness routine with advanced tracking features that monitor your progress in real-time",
+    imageURL: fitnessMobile,
+  },
+  {
+    description:
+      "Integrate your health data seamlessly with any mobile device to stay proactive about your well-being.",
+    imageURL: healthMobile,
+  },
+  {
+    description:
+      "Style meets innovation with devices designed to enhance every aspect of your lifestyle.",
+    imageURL: styleMobile,
   },
 ];
