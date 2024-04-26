7544b3f952eadbeadc330d6f5443953588aa2c40
Caroline Le Ny <caroline.lenygoyat@gmail.com>
2024-04-25 21:04:25 -0400

    ShippingInfo updated

--- client/src/pages/Checkout/ShippingInfo.js
+++ client/src/pages/Checkout/ShippingInfo.js
@@ -8,111 +8,150 @@ import {
 } from "./StyledComponents";
 import styled from "styled-components";
 import { LoggedInUserContext } from "../LoginSignUp/LoggedInUserContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 
 const ShippingInfo = () => {
   const { loggedInUser, isAuthenticated } = useContext(LoggedInUserContext);
+  //If user not log in then will store the info the user in this state
+  const [userInfo, setUserInfo] = useState({
+    country: "",
+    province: "",
+    city: "",
+    address: "",
+    postcode: "",
+    phone: "",
+    fname: "",
+    lname: "",
+    email: "",
+  });
+//Handle the changement in the input of the form and store them in the state
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserInfo({
+      ...userInfo,
+      [name]: value,
+    });
+  };
 
   return (
     <>
-      {!isAuthenticated ? (
-        <SignUpLogin to='/login'>Sign up or login before proceeding to checkout</SignUpLogin>
-      ) : (
-        <ShippingInfoContainer>
-          <Header>Shipping Information</Header>
+      <ShippingInfoContainer>
+        <Header>Shipping Information</Header>
 
-          <FormGroup>
-            <Label htmlFor="country">Country:</Label>
-            <Country name="country" id="country" value={loggedInUser.user.country} required></Country>
-          </FormGroup>
+        <FormGroup>
+          <Label htmlFor="country">Country:</Label>
+          <Country
+            name="country"
+            id="country"
+            value={isAuthenticated ? loggedInUser.user.country : userInfo.country}
+            onChange={handleChange}
+            required
+          />
+        </FormGroup>
 
-          <FormGroup>
-            <Label htmlFor="province-state">Province / State:</Label>
-            <Input
-              type="text"
-              name="province-state"
-              id="province-state"
-              value={loggedInUser.user.province}
-              required
-            /></FormGroup>
+        <FormGroup>
+          <Label htmlFor="province-state">Province / State:</Label>
+          <Input
+            type="text"
+            name="province"
+            id="province-state"
+            value={isAuthenticated ? loggedInUser.user.province : userInfo.province}
+            onChange={handleChange}
+            required
+          />
+        </FormGroup>
 
-          <FormGroup>
-            <Label htmlFor="city">City:</Label>
-            <Input
-              type="text"
-              name="city"
-              id="city"
-              value={loggedInUser.user.city}
-              required
-            /></FormGroup>
+        <FormGroup>
+          <Label htmlFor="city">City:</Label>
+          <Input
+            type="text"
+            name="city"
+            id="city"
+            value={isAuthenticated ? loggedInUser.user.city : userInfo.city}
+            onChange={handleChange}
+            required
+          />
+        </FormGroup>
 
-          <FormGroup>
-            <Label htmlFor="address">Address:</Label>
-            <Input
-              type="text"
-              name="address"
-              id="address"
-              value={loggedInUser.user.address}
-              required
-            /></FormGroup>
+        <FormGroup>
+          <Label htmlFor="address">Address:</Label>
+          <Input
+            type="text"
+            name="address"
+            id="address"
+            value={isAuthenticated ? loggedInUser.user.address : userInfo.address}
+            onChange={handleChange}
+            required
+          />
+        </FormGroup>
 
-          <FormGroup>
-            <Label htmlFor="zip">Zip/Postal Code:</Label>
-            <Input
-              type="text"
-              name="zip"
-              id="zip"
-              value={loggedInUser.user.postcode}
-              required
-            /></FormGroup>
+        <FormGroup>
+          <Label htmlFor="zip">Zip/Postal Code:</Label>
+          <Input
+            type="text"
+            name="postcode"
+            id="zip"
+            value={isAuthenticated ? loggedInUser.user.postcode : userInfo.postcode}
+            onChange={handleChange}
+            required
+          />
+        </FormGroup>
 
-          <FormGroup>
-            <Label htmlFor="phone">Phone Number:</Label>
-            <Input
-              type="tel"
-              name="phone"
-              id="phone"
-              value={loggedInUser.user.phone}
-              required
-            /></FormGroup>
+        <FormGroup>
+          <Label htmlFor="phone">Phone Number:</Label>
+          <Input
+            type="tel"
+            name="phone"
+            id="phone"
+            value={isAuthenticated ? loggedInUser.user.phone : userInfo.phone}
+            onChange={handleChange}
+            required
+          />
+        </FormGroup>
 
+        <FormGroup>
+          <Label htmlFor="fname">First Name:</Label>
+          <Input
+            type="text"
+            name="fname"
+            id="fname"
+            value={isAuthenticated ? loggedInUser.user.fname : userInfo.fname}
+            onChange={handleChange}
+            required
+          />
+        </FormGroup>
 
-          <FormGroup>
-            <Label htmlFor="fname">First Name:</Label>
-            <Input
-              type="text"
-              name="fname"
-              id="fname"
-              value={loggedInUser.user.fname}
-              required
-            /></FormGroup>
+        <FormGroup>
+          <Label htmlFor="lname">Last Name:</Label>
+          <Input
+            type="text"
+            name="lname"
+            id="lname"
+            value={isAuthenticated ? loggedInUser.user.lname : userInfo.lname}
+            onChange={handleChange}
+            required
+          />
+        </FormGroup>
 
-          <FormGroup>
-            <Label htmlFor="lname">Last Name:</Label>
-            <Input
-              type="text"
-              name="lname"
-              id="lname"
-              value={loggedInUser.user.lname}
-              required
-            /></FormGroup>
-
-          <FormGroup>
-            <Label htmlFor="email">Email:</Label>
-            <Input
-              type="email"
-              name="email"
-              id="email"
-              value={loggedInUser.user.email}
-              required
-            /></FormGroup>
-        </ShippingInfoContainer>
-      )}
+        <FormGroup>
+          <Label htmlFor="email">Email:</Label>
+          <Input
+            type="email"
+            name="email"
+            id="email"
+            value={isAuthenticated ? loggedInUser.user.email : userInfo.email}
+            onChange={handleChange}
+            required
+          />
+        </FormGroup>
+      </ShippingInfoContainer>
     </>
   );
 };
+
 export default ShippingInfo;
+
 
 const SignUpLogin = styled(Link)`
   border-radius: 4px;
