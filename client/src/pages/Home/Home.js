cdb1841a81406c0dc43e2f9c512d4360ee33a9a5
Mishel Ataev <52114376+MishelAtaev@users.noreply.github.com>
2024-04-27 21:12:34 -0400

    Update Home.js
    Removed comments

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -8,15 +8,6 @@ const Home = () => {
   useEffect(() => {
     document.title = "E-Wear Emporium";
   }, []);
-  // BASIC LAYOUT :
-  // [ ] navbar
-  // [ ] hero section, button redirects to PRODUCTS
-  // [ ] categories, also redirects to PRODUCTS > to pre-selected category
-  // [ ] rotating brands carousel, icons link to external brand websites,“see all” button links to BRANDS
-  // [ ] footer section
-  // [ ] cart
-  // [ ] email-signup/promo pop-up
-
   return (
     <>
       <NavBar />
