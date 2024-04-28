cc785c32f2cbdba1b0d6c205cbc7e99934832b35
Mishel Ataev <52114376+MishelAtaev@users.noreply.github.com>
2024-04-28 11:39:08 -0400

    Update index.js
    reverting change

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 
 //require the handlers
 const { getProductsHandler, getProductHandler, getBrandsHandler, getLoginHandler, newUserHandler, createOrderHandler, getItemHandler, deleteItemHandler} = require("./handlers");
