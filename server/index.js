c8cdd14da2e02b79ff5e0b894df8ef59c9fba672
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2025-02-15 22:30:11 -0500

    cors npm test

--- server/index.js
+++ server/index.js
@@ -2,63 +2,62 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 4000;
 
-//require the handlers
-const { getProductsHandler, getProductHandler, getBrandsHandler, getLoginHandler, newUserHandler, createOrderHandler, getItemHandler, deleteItemHandler} = require("./handlers");
+// Require the handlers
+const { getProductsHandler, getProductHandler, getBrandsHandler, getLoginHandler, newUserHandler, createOrderHandler, getItemHandler, deleteItemHandler } = require("./handlers");
 
-express()
-  .use(function (req, res, next) {
-    res.header(
-      'Access-Control-Allow-Methods',
-      'OPTIONS, HEAD, GET, PUT, POST, DELETE'
-    );
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-  })
-  .use(morgan('tiny'))
-  .use(express.static('./server/assets'))
-  .use(express.json())
-  .use(express.urlencoded({ extended: false }))
-  .use('/', express.static(__dirname + '/'))
+const app = express();
 
-  // PRODUCT ENDPOINTS___________________________________________________
+// Use CORS middleware
+app.use(cors());
 
-  //  getProductsHandler -> returns all items from db
-  .get('/products', getProductsHandler)
+app.use(function (req, res, next) {
+  res.header(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, HEAD, GET, PUT, POST, DELETE'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
-  //  getProductHandler -> returns specific item from db using _id
-  .get("/products/:_id", getProductHandler)
-  
+app.use(morgan('tiny'));
+app.use(express.static('./server/assets'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/', express.static(__dirname + '/'));
 
+// PRODUCT ENDPOINTS___________________________________________________
 
+// getProductsHandler -> returns all items from db
+app.get('/products', getProductsHandler);
 
-  // USER ENDPOINTS___________________________________________________
+// getProductHandler -> returns specific item from db using _id
+app.get("/products/:_id", getProductHandler);
 
-  // newUserHandler -> posts a new user to db using id
-  .post("/signUp", newUserHandler)
-  // logInHandler -> retrieves user from db logs user in
-  .post("/login", getLoginHandler)
+// USER ENDPOINTS___________________________________________________
 
+// newUserHandler -> posts a new user to db using id
+app.post("/signUp", newUserHandler);
+// logInHandler -> retrieves user from db logs user in
+app.post("/login", getLoginHandler);
 
-  // ORDER ENDPOINTS___________________________________________________
+// ORDER ENDPOINTS___________________________________________________
 
-  // createOrderHandler -> creates order from cart and adds to db
-  .post("/checkout", createOrderHandler)
-  // getItemHandler -> update the stock according to what the user added
-  .patch("/products/:_id", getItemHandler)
-  // deleteItemHandler -> update the stock if the user delete the item from his cart
-  .patch("/checkout", deleteItemHandler)
+// createOrderHandler -> creates order from cart and adds to db
+app.post("/checkout", createOrderHandler);
+// getItemHandler -> update the stock according to what the user added
+app.patch("/products/:_id", getItemHandler);
+// deleteItemHandler -> update the stock if the user delete the item from his cart
+app.patch("/checkout", deleteItemHandler);
 
+// getBrands -> retrieves all brands from db
+app.get("/brands", getBrandsHandler);
 
-
-  // getBrands -> retrieves all brands from db
-  .get("/brands", getBrandsHandler)
-
-
-  //_______________________________________________________________
-  .listen(PORT, () => console.info(`Listening on port ${PORT}`));
+//_______________________________________________________________
+app.listen(PORT, () => console.info(`Listening on port ${PORT}`));
