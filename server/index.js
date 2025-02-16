d7614be46aa5b6a44224b17a8c771c745e238e61
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2025-02-15 23:05:17 -0500

    test26

--- server/index.js
+++ server/index.js
@@ -8,10 +8,20 @@ const PORT = process.env.PORT || 4000;
 //require the handlers
 const { getProductsHandler, getProductHandler, getBrandsHandler, getLoginHandler, newUserHandler, createOrderHandler, getItemHandler, deleteItemHandler} = require("./handlers");
 
-
-
-express()
-  .use(cors())
+const app = express()
+  
+app.use(cors())
+  .use(function (req, res, next) {
+    res.header(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, HEAD, GET, PUT, POST, DELETE'
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  })
   .use(morgan('tiny'))
   .use(express.static('./server/assets'))
   .use(express.json())
@@ -26,16 +36,12 @@ express()
   //  getProductHandler -> returns specific item from db using _id
   .get("/products/:_id", getProductHandler)
   
-
-
-
   // USER ENDPOINTS___________________________________________________
 
   // newUserHandler -> posts a new user to db using id
   .post("/signUp", newUserHandler)
   // logInHandler -> retrieves user from db logs user in
   .post("/login", getLoginHandler)
-
 
   // ORDER ENDPOINTS___________________________________________________
 
@@ -46,11 +52,8 @@ express()
   // deleteItemHandler -> update the stock if the user delete the item from his cart
   .patch("/checkout", deleteItemHandler)
 
-
-
   // getBrands -> retrieves all brands from db
   .get("/brands", getBrandsHandler)
-
 
   //_______________________________________________________________
   .listen(PORT, () => console.info(`Listening on port ${PORT}`));
