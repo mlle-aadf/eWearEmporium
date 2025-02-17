a889dc8031fbde125ca0bc3fa898001209dd5246
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2025-02-16 19:55:02 -0500

    rt test

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 4000;
 const { getProductsHandler, getProductHandler, getBrandsHandler, getLoginHandler, newUserHandler, createOrderHandler, getItemHandler, deleteItemHandler} = require("./handlers");
 
 
+
 express()
   .use(function (req, res, next) {
     res.header(
@@ -27,41 +28,45 @@ express()
   .use(express.static(path.resolve(__dirname, '../client/build')))
   .use(express.json())
   .use(express.urlencoded({ extended: false }))
-  .use('/', express.static(__dirname + '/'))
-
+  // .use('/api', express.static(__dirname + '/'))
+  
   // PRODUCT ENDPOINTS___________________________________________________
-
+  
   //  getProductsHandler -> returns all items from db
   .get('/api/products', getProductsHandler)
-
+  
   //  getProductHandler -> returns specific item from db using _id
   .get("/api/products/:_id", getProductHandler)
   
-
-
-
+  
+  
+  
   // USER ENDPOINTS___________________________________________________
-
+  
   // newUserHandler -> posts a new user to db using id
   .post("/api/signUp", newUserHandler)
   // logInHandler -> retrieves user from db logs user in
   .post("/api/login", getLoginHandler)
-
-
+  
+  
   // ORDER ENDPOINTS___________________________________________________
-
+  
   // createOrderHandler -> creates order from cart and adds to db
   .post("/api/checkout", createOrderHandler)
   // getItemHandler -> update the stock according to what the user added
   .patch("/api/products/:_id", getItemHandler)
   // deleteItemHandler -> update the stock if the user delete the item from his cart
   .patch("/api/checkout", deleteItemHandler)
-
-
-
+  
+  
+  
   // getBrands -> retrieves all brands from db
   .get("/api/brands", getBrandsHandler)
-
-
+  
+  
+  .get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  })
   //_______________________________________________________________
   .listen(PORT, () => console.info(`Listening on port ${PORT}`));
+  
