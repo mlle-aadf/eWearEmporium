a0797d91d1d3a7477e1e7e83ed6d657d1cf410f8
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2025-02-17 00:35:21 -0500

    contact

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -1,9 +1,8 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
-import NavBar from "../../components/NavBar";
-import Footer from "../../components/Footer";
 import styled from "styled-components";
+import Footer from "../../components/Footer";
+import NavBar from "../../components/NavBar";
 
 //This page contains a form for users to write their questions/comments
 const Contact = () => {
@@ -39,109 +38,141 @@ const Contact = () => {
   return (
     <>
       <NavBar />
-      <Main>
-        <Div>
-        <Form>
-          <H1>Let's Get In Touch!</H1>
-          <br />
+      <Container>
+        <FormContainer>
+          <Title>Contact Us</Title>
           <Subtext>
-            For any questions or inquiries, please feel free to leave a message.{" "}
-            <br /> Thank you!
+            Got questions or feedback? We’re just a message away! Our customer
+            support team is here to help with anything you need.
           </Subtext>
-          <br />
-          <br />
-          <Label>Your First Name: </Label>
-          <br />
-          <input
-            className="input-field"
-            type="text"
-            value={firstName}
-            onChange={userFirstName}
-            placeholder="Your first name"
-          ></input>
-          <br />
-          <br />
-          <Label>Your Last Name: </Label>
-          <br />
-          <input
-            className="input-field"
-            type="text"
-            value={lastName}
-            onChange={userLastName}
-            placeholder="Your last name"
-          ></input>
-          <br />
-          <br />
-          <Label>Your Email: </Label>
-          <br />
-          <input
-            className="input-field"
-            type="text"
-            value={email}
-            onChange={userEmail}
-            placeholder="Your email"
-          ></input>
-          <br />
-          <br />
-          <Label>Your Message: </Label>
-          <br />
-          <input
-            className="msg-field"
-            type="text"
-            value={message}
-            onChange={userMessage}
-            placeholder="Your message"
-          ></input>
-          <br />
-          <br />
-          <Link
-            to="/contact-success"
-            style={{ color: "black", textDecoration: "none" }}
-          >
-            <button className="contact-btn" type="submit" disabled={blankInput}>
-              Submit
-            </button>
-          </Link>
-          <br />
-        </Form>
-        </Div>
-      </Main>
+          <Form>
+            <FormGroup>
+              <Label>Your First Name:</Label>
+              <Input
+                type="text"
+                value={firstName}
+                onChange={userFirstName}
+                placeholder="Your first name"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label>Your Last Name:</Label>
+              <Input
+                type="text"
+                value={lastName}
+                onChange={userLastName}
+                placeholder="Your last name"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label>Your Email:</Label>
+              <Input
+                type="email"
+                value={email}
+                onChange={userEmail}
+                placeholder="Your email"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label>Your Message:</Label>
+              <MessageInput
+                value={message}
+                onChange={userMessage}
+                placeholder="Your message"
+              />
+            </FormGroup>
+            <Link
+              to="/contact-success"
+              style={{ color: "black", textDecoration: "none" }}
+            >
+              <Button type="submit" disabled={blankInput}>
+                Submit
+              </Button>
+            </Link>
+          </Form>
+        </FormContainer>
+      </Container>
       <Footer />
     </>
   );
 };
 
-const Main = styled.main`
-  margin: 150px 0 100px 0;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding: 0 20px;
+  margin-top: 4vh;
+
+  @media (max-width: 500px) {
+    margin-top: 0;
+  }
+`;
+
+const FormContainer = styled.div`
+  background-color: white;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  max-width: 600px;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+const Subtext = styled.p`
+  text-align: center;
+  margin-bottom: 30px;
 `;
 
 const Form = styled.form`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
 `;
 
-const H1 = styled.h1`
-  text-align: center;
-  text-decoration: underline;
-  font-weight: bold;
-  font-size: xx-large;
-`;
-
-const Subtext = styled.h3`
-  text-align: center;
-  font-weight: bold;
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
 `;
 
 const Label = styled.label`
   font-weight: bold;
+  margin-bottom: 5px;
 `;
 
-const Div = styled.div`
-  background-color: white;
-  outline-style: solid;
-  outline-color: black;
-  border-radius: 10px;
-  padding: 75px;
-  margin: 0 50px 0 50px;
+const Input = styled.input`
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+const MessageInput = styled.textarea`
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  resize: vertical;
+  height: 100px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: #007bff;
+  color: white;
+  cursor: pointer;
+  margin-top: 20px;
+
+  &:disabled {
+    background-color: #ccc;
+    cursor: not-allowed;
+  }
 `;
 
 export default Contact;
